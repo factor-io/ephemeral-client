ec69f926a9599ce1389b5db8a0ef1f7653f57e4b
Clem <clemfree@gmail.com>
2015-04-28 14:50:54 -0700

    Add test for returned id

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -11,4 +11,10 @@ describe 'Ephemeral::Client' do
       test_client.build
     }.to raise_error ArgumentError
   end
+
+  it 'returns an id' do
+    test_client = Ephemeral::Client.new
+    response = test_client.build("test", "test", "test")
+    expect(response).to have_type String
+  end
 end
