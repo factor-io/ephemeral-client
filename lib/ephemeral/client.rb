fe41dcf981ed9296758f39b985f0e9e9a4257868
Clem <clemfree@gmail.com>
2015-04-28 14:50:35 -0700

    Add restclient POST code

--- lib/ephemeral/client.rb
+++ lib/ephemeral/client.rb
@@ -1,9 +1,15 @@
-
+require 'rest-client'
 
 module Ephemeral
   class Client
     def build(image, repo, command)
 
+      response = RestClient.post('http://ephemeral.io/build',
+        {
+          image:   image,
+          repo:    repo,
+          command: command
+      })
     end
   end
 end
