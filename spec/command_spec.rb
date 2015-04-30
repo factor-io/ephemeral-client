1af5b2a9f4c82a944f5fd8d25997373a2f974196
Maciej Skierkowski <maciej@skierkowski.com>
2015-04-29 21:34:25 -0700

    fixing requires in test

--- spec/command_spec.rb
+++ spec/command_spec.rb
@@ -1,7 +1,8 @@
 # encoding: UTF-8
 
 require 'spec_helper'
-require 'ephemeral/command'
+require 'ephemeral/commands/build'
+require 'commander'
 require 'securerandom'
 
 describe Ephemeral::Commands do
