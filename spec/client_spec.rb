3e0b54d60eca207520839c740b30e5af66eba668
Clem <clemfree@gmail.com>
2015-05-10 13:37:28 -0700

    Add build group to tests

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -5,22 +5,24 @@ require 'ephemeral/client'
 
 describe 'Ephemeral::Client' do
   
-  it 'expects 3 arguments' do
-    test_client = Ephemeral::Client.new
-    expect{
-      test_client.build
-    }.to raise_error ArgumentError
-  end
+  describe 'build' do
+    it 'expects 3 arguments' do
+      test_client = Ephemeral::Client.new
+      expect{
+        test_client.build
+      }.to raise_error ArgumentError
+    end
 
-  it 'returns a hash' do
-    test_client = Ephemeral::Client.new
-    response = test_client.build("ruby:2.1", "https://github.com/skierkowski/hello-middleman", "middleman")
-    expect(response).to be_an_instance_of Hash
-  end
+    it 'returns a hash' do
+      test_client = Ephemeral::Client.new
+      response = test_client.build("ruby:2.1", "https://github.com/skierkowski/hello-middleman", "middleman")
+      expect(response).to be_an_instance_of Hash
+    end
 
-  it 'returns a hash which contains an id' do
-    test_client = Ephemeral::Client.new
-    response = test_client.build("ruby:2.1", "https://github.com/skierkowski/hello-middleman", "middleman")
-    expect(response).to have_key('id')
+    it 'returns a hash which contains an id' do
+      test_client = Ephemeral::Client.new
+      response = test_client.build("ruby:2.1", "https://github.com/skierkowski/hello-middleman", "middleman")
+      expect(response).to have_key('id')
+    end
   end
 end
