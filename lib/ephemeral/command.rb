894d1894e50387f139bdf680f00db518b243d8c7
Clem <clemfree@gmail.com>
2015-05-18 11:11:45 -0700

    Add logs command to commander file

--- lib/ephemeral/command.rb
+++ lib/ephemeral/command.rb
@@ -3,6 +3,8 @@
 require 'commander/import'
 require 'ephemeral/version'
 require 'ephemeral/commands/build'
+require 'ephemeral/commands/logs'
+
 
 program :name, 'Ephemeral Client'
 program :version, Ephemeral::VERSION
@@ -10,8 +12,14 @@ program :description, 'Eph (eph) is a command line tool to run builds on the Eph
 
 command 'build' do |c|
   c.syntax = 'eph source_url [--buildpack ...] [--framework ...]'
-  c.option '--buildpack STRING', String, 'The name of the buildpace to use from https://registry.hub.docker.com/_/buildpack-deps/'
+  c.option '--buildpack STRING', String, 'The name of the buildpack to use from https://registry.hub.docker.com/_/buildpack-deps/'
   c.option '--framework STRING', String, 'The framework to use for building the service (e.g. middleman, jekyll)'
   c.description = 'Builds the code hosted on source using the builder framework on the runtime specified'
   c.when_called Ephemeral::Commands::Build, :build
+end
+
+command 'logs' do |c|
+  c.syntax = 'eph build_id'
+  c.description = 'Gets the logs from an Ephemeral build.'
+  c.when_called Ephemeral::Commands::Logs, :logs
 end
